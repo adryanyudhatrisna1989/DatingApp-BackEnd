4fb1174e8ccb62761ab5837873b207c3b63e7d72
iamadryan <ayudhatrisna1989@gmail.com>
2018-07-05 18:58:45 +0700

    Implemented authentication using JWT

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers {
+  [Authorize]
   [Route ("api/[controller]")]
   public class ValuesController : Controller {
     private readonly DataContext _context;
@@ -14,6 +16,7 @@ namespace DatingApp.API.Controllers {
       _context = context;
 
     }
+
     // GET api/values
     [HttpGet]
     public async Task<IActionResult> GetValues () {
