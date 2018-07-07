f8f108231500664b2fa2ac7340d1720ceb3087c5
iamadryan <ayudhatrisna1989@gmail.com>
2018-07-07 13:56:25 +0700

    Extended API

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -30,9 +31,11 @@ namespace DatingApp.API {
         public void ConfigureServices (IServiceCollection services) {
             var key = Encoding.ASCII.GetBytes (Configuration.GetSection ("AppSettings:Token").Value);
             services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
-            services.AddMvc ();
+            services.AddTransient<Seed> ();
             services.AddCors ();
+            services.AddAutoMapper ();
             services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddScoped<IDatingRepository, DatingRepository> ();
             services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer (options => {
                     options.TokenValidationParameters = new TokenValidationParameters {
@@ -42,10 +45,13 @@ namespace DatingApp.API {
                     ValidateAudience = false
                     };
                 });
+            services.AddMvc ().AddJsonOptions (opt => {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, Seed seeder) {
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
             } else {
@@ -60,6 +66,8 @@ namespace DatingApp.API {
                     });
                 });
             }
+
+            //seeder.SeeUsers ();
 
             app.UseCors (x => x.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ().AllowCredentials ());
             app.UseAuthentication ();
